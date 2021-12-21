7c22f23dc1ac3b75b0711d90d90a411dbb16c61e
hotpot <2240379431@qq.com>
2021-12-21 17:21:25 +0800

    change index.js

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
+const { FILE } = require('dns')
 const fs = require('fs')
 const path = require('path')
 
-const notGenerateList = ['.git', 'index.js', 'package.json', 'README.md']
 let template = (title, link) => `- [${title}](./${link})  
 `
-
+const notGenerateList = ['.git', 'index.js', 'package.json', 'README.md']
 const rootDir = fs.readdirSync(__dirname)
-const list = rootDir.filter((_) => !notGenerateList.includes(_))
 
-for (let dir of list) {
-  const res = fs.readdirSync(path.resolve(__dirname, dir))
-  //   console.log(res)
-  //   console.log(`${__dirname}/${dir}`)
-  fs.writeFileSync(`${__dirname}/${dir}/readme.md`, '')
-  if (res.length > 0) {
-    let content = []
-    res.forEach((file) => {
-      if (file !== 'readme.md') {
-        fs.appendFileSync(`${__dirname}/${dir}/readme.md`, template(file, file))
-      }
-    })
+const GenerateArticleDir = () => {
+  const list = rootDir.filter((_) => !notGenerateList.includes(_))
+
+  for (let dir of list) {
+    const res = fs.readdirSync(path.resolve(__dirname, dir))
+    fs.writeFileSync(`${__dirname}/${dir}/readme.md`, '')
+    if (res.length > 0) {
+      let content = []
+      res.forEach((file) => {
+        if (file !== 'readme.md') {
+          fs.appendFileSync(
+            `${__dirname}/${dir}/readme.md`,
+            template(file, file)
+          )
+        }
+      })
+    }
   }
 }
+
+GenerateArticleDir()
